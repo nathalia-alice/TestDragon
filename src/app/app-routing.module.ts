eadb21d0c2bca855111834f57b341f80059c03b0
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 10:59:42 -0300

    Create Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DragonsListingComponent } from './components/dragons/dragons-listing/dragons-listing.component';
+import { DragonsDetailsComponent } from './components/dragons/dragons-details/dragons-details.component';
+import { DragonsCreateComponent } from './components/dragons/dragons-create/dragons-create.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'dragon/create', //rota página criar dragão
+    component: DragonsCreateComponent }, 
+
+  { path: 'dragon/:id', //rota página detalhes do dragão
+   component: DragonsDetailsComponent }, 
+
+  { path: 'dragons', //rota página listar dragões
+    component: DragonsListingComponent },
+
+  { path: 'login', //rota página login
+    component: LoginComponent },
+
+  { path: '',  //rota página login
+    component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
