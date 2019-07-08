95e7ff392a873ff08aea71431b3ae3374a5ac746
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 00:22:49 -0300

    Login with/without Facebook

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
+declare var FB: any;
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit() {
+    //login com facebook
+    (window as any).fbAsyncInit = function () {
+      FB.init({
+        appId: '1082805611928492',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.3'
+      });
+      FB.AppEvents.logPageView();
+    };
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) { return; }
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    //login sem facebook
+    this.getForm();
   }
 
+  getForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login() {
+    if (this.form.invalid) {
+      this.snackBar.open(
+        "Dados inválidos", "Erro", { duration: 5000 });
+    } else {
+      //se houvesse api login e retornasse success redireciona page list dragons
+      this.router.navigateByUrl('/dragons');
+    }
+  }
+
+  submitLogin() {
+
+    FB.login((response) => {
+      if (response.authResponse) {
+        this.router.navigateByUrl('/dragons');
+      } else {
+        this.snackBar.open(
+          "Falha no login do usuário", "Erro", { duration: 5000 });
+      }
+    });
+  }
 }
+
+
+
+
+
+
+
