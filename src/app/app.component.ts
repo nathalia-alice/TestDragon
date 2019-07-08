fbd9a34d3fe7f97039c28dd6fa53494945c16300
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 11:10:21 -0300

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare var FB: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
   title = 'TestDragon';
+
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+
+  }
+
+  deslogar() {
+    FB.logout();
+    this.router.navigateByUrl('/');
+  }
 }
+
