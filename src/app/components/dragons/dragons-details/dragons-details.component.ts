becc984da79cf42ab8ad3fd9a61ad556d824f540
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 11:12:10 -0300

    Component Dragons Details

--- src/app/components/dragons/dragons-details/dragons-details.component.ts
+++ src/app/components/dragons/dragons-details/dragons-details.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { DragonService } from '../../../services/dragon.service';
 
 @Component({
   selector: 'app-dragons-details',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragonsDetailsComponent implements OnInit {
 
-  constructor() { }
+  dragon;
+  title = "Detalhes";
+  
+  constructor(
+    private route: ActivatedRoute,
+    private dragonService: DragonService,
+  ) { }
 
   ngOnInit() {
+    const dragonId = this.route.snapshot.paramMap.get('id');
+    this.dragonService.getDragon(dragonId).subscribe(response => {
+      this.dragon = response;
+    });
   }
-
 }
