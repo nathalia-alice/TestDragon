10b1232fd3dd9899ab7f8907ea26910fdf12e0af
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 11:09:26 -0300

    Update Environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseApiUrl: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
+  baseApiDragon: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon',
   baseUrl: 'http://localhost:4200/'
 };
