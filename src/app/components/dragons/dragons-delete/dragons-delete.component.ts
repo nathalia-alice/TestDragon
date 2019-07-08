40983d3af8633a5f34d285173b6509d495a3ac98
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 11:12:32 -0300

    Component Dragons Delete

--- src/app/components/dragons/dragons-delete/dragons-delete.component.ts
+++ src/app/components/dragons/dragons-delete/dragons-delete.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatSnackBar, MatDialogRef } from '@angular/material';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { DragonModel } from '../../../models/dragon.model';
+import { DragonService } from '../../../services/dragon.service';
+
 
 @Component({
   selector: 'app-dragons-delete',
   templateUrl: './dragons-delete.component.html',
   styleUrls: ['./dragons-delete.component.scss']
 })
+
 export class DragonsDeleteComponent implements OnInit {
+  title = "Excluir Dragão";
 
-  constructor() { }
+  constructor(
+    private dragonService: DragonService,
+    public snackBar: MatSnackBar,
+    public dialogDeleteDragon: MatDialogRef<DragonsDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public dialog: DragonModel,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  deleteDragon() {
+    //mensagem após concluir solicitação de excluir dragão
+    this.dragonService.deleteDragon(this.dialog).subscribe(() => {
+      this.snackBar.open(
+       "Dragão excluído com sucesso.", "Sucesso", { duration: 5000 });
+       this.dialogDeleteDragon.close();
+    },
+    (err: HttpErrorResponse) => {
+      this.snackBar.open(
+        "Houve um erro ao excluir o dragão.", "Erro", { duration: 5000 });
+    });
   }
-
 }
+
