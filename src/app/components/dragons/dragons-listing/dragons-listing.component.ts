3d92408bf4c140a76a8809713fe2c8a971c298bc
nathalia-alice <nathalia.alice.silv@gmail.com>
2019-07-08 11:11:39 -0300

    Component Dragons Listing

--- src/app/components/dragons/dragons-listing/dragons-listing.component.ts
+++ src/app/components/dragons/dragons-listing/dragons-listing.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort, MatDialog, MatSnackBar } from '@angular/material';
+
+import { DragonService } from '../../../services/dragon.service';
+import { DragonsEditComponent } from '../dragons-edit/dragons-edit.component';
+import { DragonsDeleteComponent } from '../dragons-delete/dragons-delete.component';
 
 @Component({
   selector: 'app-dragons-listing',
   templateUrl: './dragons-listing.component.html',
   styleUrls: ['./dragons-listing.component.scss']
 })
+
 export class DragonsListingComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  sortDragons;
+  dragons;
+  columnsTable: string[] = ['nome', 'tipo', 'data-criacao', 'editar', 'excluir'];
+
+  constructor(
+    private dragonService: DragonService,
+    private materialDialog: MatDialog
+  ) { }
 
   ngOnInit() {
+    this.getListDragons();
   }
 
+  getListDragons() {
+    this.dragonService.getDragons().subscribe(response => {
+      //fazendo ordenação e paginação lista dragões
+      this.sortDragons = response;
+      this.sortDragons.sort((a, b) => a.name.localeCompare(b.name));
+      this.dragons = new MatTableDataSource(this.sortDragons);
+      this.dragons.paginator = this.paginator;
+
+    });
+  }
+
+  deleteDragon(dragon) {
+    const dialog = this.materialDialog.open(DragonsDeleteComponent, {
+      width: '500px',
+      height: '500px;',
+      data: {
+        id: dragon.id,
+        name: dragon.name,
+        type: dragon.type,
+        createdAt: dragon.createdAt
+      }
+    });
+
+    //depois de fechar modal
+    this.afterClosedModal(dialog);
+  }
+
+  editDragon(dragon) {
+    const dialog = this.materialDialog.open(DragonsEditComponent, {
+      width: '500px',
+      height: '500px;',
+      data: {
+        id: dragon.id,
+        name: dragon.name,
+        type: dragon.type,
+        createdAt: dragon.createdAt,
+        histories: dragon.histories
+      }
+    });
+
+    //depois de fechar modal
+    this.afterClosedModal(dialog);
+
+  }
+
+  afterClosedModal(dialog) {
+    dialog.afterClosed().subscribe(() => {
+      setTimeout(() => {
+        this.getListDragons();
+      }, 100);
+    });
+  }
 }
